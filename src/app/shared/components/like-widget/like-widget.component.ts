ad6f9e716f16d267799f36d9b5a0d0915b872935
Rafael Aranda Martins <skema1114@hotmail.com>
2021-06-21 10:46:47 -0300

    Terminando a parte 04

--- src/app/shared/components/like-widget/like-widget.component.ts
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+
 import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 
 @Component({
