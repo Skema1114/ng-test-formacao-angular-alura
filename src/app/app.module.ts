6a9c810655560387b4f87b2cc18de60a00d5638b
Rafael Aranda Martins <skema1114@hotmail.com>
2021-06-21 15:56:41 -0300

    Iniciando o sétimo curso da formação em angular da alura e terminando a parte 01 do mesmo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './component/app.component';
 import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
+import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LikeWidgetModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LikeWidgetModule,
+    PhotoFrameModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
